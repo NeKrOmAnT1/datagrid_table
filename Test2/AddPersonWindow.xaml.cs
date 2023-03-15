a5458f560c347f9fbded9759bc7b0b5b1091a820
NeKrOmAnT1 <miftahov9548@gmail.com>
2023-03-15 16:58:21 +0300

    Добавьте файлы проекта.

---
+++ Test2/AddPersonWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test2
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPersonWindow.xaml
+    /// </summary>
+    public partial class AddPersonWindow : Window
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public AddPersonWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            FirstName = tb_firstname.Text;
+            LastName = tb_lastname.Text;
+            DialogResult = true;
+        }
+    }
+}
---
+++ Test2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test2
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Person> people = new ObservableCollection<Person>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+                data_grid.ItemsSource = people;
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            AddPersonWindow add = new AddPersonWindow();
+            if (add.ShowDialog() == true)
+            {
+                people.Add(new Person { FirstName = add.FirstName, LastName = add.LastName });
+            }
+
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                people.Remove((Person)data_grid.SelectedItem);
+            }
+            catch
+            {
+                MessageBox.Show("Больше так не делай");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
